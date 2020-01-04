9ef4ddbd54eb6dbe84fbc2a2788c1225bede0e61
AhVen98 <paola.costa98@gmail.com>
2020-01-04 10:52:45 +0100

    tous les docs car rien ne marche pour commit

---
+++ cmake-build-debug/CMakeFiles/BattleShip.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "C:/Users/paola/Desktop/BattleShip/aide.c" "C:/Users/paola/Desktop/BattleShip/cmake-build-debug/CMakeFiles/BattleShip.dir/aide.c.obj"
+  "C:/Users/paola/Desktop/BattleShip/grille.c" "C:/Users/paola/Desktop/BattleShip/cmake-build-debug/CMakeFiles/BattleShip.dir/grille.c.obj"
+  "C:/Users/paola/Desktop/BattleShip/jouer.c" "C:/Users/paola/Desktop/BattleShip/cmake-build-debug/CMakeFiles/BattleShip.dir/jouer.c.obj"
+  "C:/Users/paola/Desktop/BattleShip/main.c" "C:/Users/paola/Desktop/BattleShip/cmake-build-debug/CMakeFiles/BattleShip.dir/main.c.obj"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/BattleShip.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "BattleShip.exe"
+  "BattleShip.exe.manifest"
+  "BattleShip.pdb"
+  "CMakeFiles/BattleShip.dir/aide.c.obj"
+  "CMakeFiles/BattleShip.dir/grille.c.obj"
+  "CMakeFiles/BattleShip.dir/jouer.c.obj"
+  "CMakeFiles/BattleShip.dir/main.c.obj"
+  "libBattleShip.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/BattleShip.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
